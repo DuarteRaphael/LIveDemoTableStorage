1e7a297513c43bf1a8377b01c70bc433a0b8109b
Raphael Duarte <Raphael Duarte>
2017-04-05 15:06:48 -0300

    Adicionar arquivos de projeto.

---
+++ CargaCotacoes/Carga.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+
+namespace CargaCotacoes
+{
+    public static class Carga
+    {
+        public static void IncluirCotacao(CloudTable table, string siglaMoeda, string dataCotacao, double valorComercial)
+        {
+            CotacaoEntity cotacao = new CotacaoEntity(siglaMoeda, dataCotacao, valorComercial);
+
+            TableOperation insertOperation = TableOperation.Insert(cotacao);
+            table.Execute(insertOperation);
+        }
+        public static void ListaCotacoes<T>(CloudTable table, string siglaMoeda) where T: CotacaoEntity, new()
+        {
+            TableQuery<T> query = new TableQuery<T>().Where(TableQuery.GenerateFilterCondition("Partitionkey", QueryComparisons.Equal, siglaMoeda));
+
+            foreach (T entity in table.ExecuteQuery(query))
+            {
+                Console.Write(Environment.NewLine);
+                Console.WriteLine(JsonConvert.SerializeObject(entity));
+            }
+            Console.WriteLine(Environment.NewLine);
+        }
+    }
+}
---
+++ CargaCotacoes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+
+namespace CargaCotacoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Obter configurações de acesso...");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            Console.WriteLine("Gerar a referência da tabela e criar no storage (caso a mesma ainda não exista)...");
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference("Cotacoes");
+            table.CreateIfNotExists();
+
+            Console.WriteLine("Incluindo cotações do Euro (EUR)...");
+            Carga.IncluirCotacao(table, "EUR", "2017-03-20 16:59", 3.3016);
+            Carga.IncluirCotacao(table, "EUR", "2017-03-21 16:59", 3.3417);
+
+            Console.WriteLine("Listando cotações do Euro (EUR)...");
+            Carga.ListaCotacoes<CotacaoEntity>(table, "EUR");
+
+            Console.WriteLine("Incluindo cotações da Libra Esterlina (LIB)...");
+            Carga.IncluirCotacao(table, "LIB", "2017-03-20 16:59", 3.7979);
+            Carga.IncluirCotacao(table, "LIB", "2017-03-21 16:59", 3.8573);
+
+            Console.WriteLine("Listando cotações da Libra Esterlina (LIB)...");
+            Carga.ListaCotacoes<CotacaoEntity>(table, "LIB");
+
+            Console.WriteLine("Incluindo cotações do Dólar (USD)...");
+            TableBatchOperation batch = new TableBatchOperation();
+            batch.Insert(new CotacaoDolarEntity("USD", "2017-03-20 16:59", 3.0717, 3.2300));
+            batch.Insert(new CotacaoDolarEntity("USD", "2017-03-21 16:59", 3.0900, 3.2600));
+            table.ExecuteBatch(batch);
+
+            Console.WriteLine("Listando cotações do Dólar (USD)...");
+            Carga.ListaCotacoes<CotacaoDolarEntity>(table, "USD");
+
+            Console.WriteLine("Finalizado");
+            Console.ReadKey();
+        }
+    }
+}
